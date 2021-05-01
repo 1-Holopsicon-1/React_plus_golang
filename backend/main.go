871f10d76c3e04081432459dad6947ece1ebaa67
1-Holopsicon-1 <mfnbrx@gmail.com>
2021-05-01 13:41:21 +0300

    made some refact

--- backend/main.go
+++ backend/main.go
@@ -1,55 +1,29 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "github.com/gorilla/websocket"
+	"./pkg/websocket"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-var upgrader = websocket.Upgrader{
-  ReadBufferSize: 1024,
-  WriteBufferSize: 1024,
-  CheckOrigin: func(r *http.Request) bool {
-    return true
-  },
-  }
-
-func reader(conn *websocket.Conn)  {
-  for {
-    messageType, p, err := conn.ReadMessage()
-    if err != nil{
-      log.Println(err)
-      return
-    }
-  fmt.Println(string(p))
-    if err := conn.WriteMessage(messageType, p); err != nil{
-      log.Println(err)
-      return
-    }
-  }
-}
-
-func serveWS(w http.ResponseWriter, r *http.Request)  {
-  fmt.Println(r.Host)
-  ws, err := upgrader.Upgrade(w, r, nil)
-  if err != nil{
-    log.Println(err)
-    return
-  }
-  reader(ws)
+func serveWS(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Simple Server")
-  })
-  http.HandleFunc("/ws", serveWS)
+	http.HandleFunc("/ws", serveWS)
 }
 
 func main() {
-fmt.Println("Chat App v0.01")
-  fmt.Print("Running server \nPress ^C To cancel \n")
-  setupRoutes()
-http.ListenAndServe(":8080", nil)
+	fmt.Println("Chat App v0.01")
+	fmt.Print("Running server \nPress ^C To cancel \n")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
