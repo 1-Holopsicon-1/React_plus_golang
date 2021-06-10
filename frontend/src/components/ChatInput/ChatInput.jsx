8d41c6df80b9dc4efc0460681396deb95a0bca24
1-Holopsicon-1 <mfnbrx@gmail.com>
2021-06-10 15:10:15 +0300

    upd to previos commit

--- frontend/src/components/ChatInput/ChatInput.jsx
+++ frontend/src/components/ChatInput/ChatInput.jsx
@@ -1,35 +1,53 @@
-import React, {Component} from "react";
+import { ChevronRight, EmojiSmile } from 'react-bootstrap-icons'
+import React, {Component} from "react"
+import 'emoji-mart/css/emoji-mart.css'
+import { Picker } from 'emoji-mart'
 import "./ChatInput.sass"
-import { BoxArrowInRight } from 'react-bootstrap-icons';
 
-class ChatInput extends Component{
+export default class ChatInput extends Component{
     constructor(props) {
         super(props);
         this.btnHand = this.btnHand.bind(this)
         this.inputHand = this.inputHand.bind(this)
     }
-    btnHand(){
+
+    btnHand() {
         let text = document.querySelector("#inpt")
+        if (text.value === '') return
+
         this.props.send(text.value)
         text.value = ""
     }
 
-    inputHand(event){
+    inputHand(event) {
         if (event.code === "Enter"){
             let text = document.querySelector("#inpt")
+            if (text.value === '') return
             this.props.send(text.value)
             text.value = ""
         }
-
     }
+
+    emojiToggleHand(event) {
+        document.querySelector('.emojiMarkt').classList.toggle('hidden')
+    }
+
+    emojiHandler(emoji, event) {
+        document.querySelector("#inpt").value = document.querySelector("#inpt").value + emoji.native
+    }
+
     render() {
         return (
             <div className="ChatInput">
-                <input id="inpt" onKeyDown={this.inputHand} placeholder="Type your message..."/>
-                <div id="btn" onClick={this.btnHand}><BoxArrowInRight className="btn_send"/></div>
+                <div className="emojiAndInput">
+                    <EmojiSmile onClick={this.emojiToggleHand} className="smile"/>
+                    <div className="emojiMarkt hidden">
+                        <Picker theme="dark" onClick={this.emojiHandler}/>
+                    </div>
+                    <input id="inpt" placeholder="Message" onKeyDown={this.inputHand}/>
+                </div>
+                <div id="btn" onClick={this.btnHand}><ChevronRight className="btn_send"/></div>
             </div>
-        );
+        )
     }
 }
-
-export default ChatInput;
